a874d2b2ea75f954b87d720af182707f86f18662
latsa <latsapnev01@mail.ru>
2023-12-15 17:01:15 +0300

    Task №4.

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,12 @@ public class Main {
         if (speed > 60) {System.out.println("Если скорость "+speed+", то придется заплатить штраф.");}
         if (speed <= 60) {System.out.println("Если скорость "+speed+", то можно ездить спокойно.");}
 
+        //Task 4
+        int age4 = 10;
+        if (age4 >=2 && age4 <=6) {System.out.println("Если возраст человека равен "+age4+", то ему нужно ходить в детский сад.");}
+        else if (age4 >=7 && age4 <=17) {System.out.println("Если возраст человека равен "+age4+", то ему нужно ходить в школу.");}
+        else if (age4 >=18 && age4 <=24) {System.out.println("Если возраст человека равен "+age4+", то его место в университете.");}
+        else if (age4 >24) {System.out.println("Если возраст человека равен "+age4+", то ему пора ходить на работу.");}
+
     }
 }
