b3cd6572cea59c057b1d11877cb3c5171f462ac9
latsa <latsapnev01@mail.ru>
2023-12-15 16:59:20 +0300

    Task №1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1
+        int age = 17;
+        if (age >= 18) {System.out.println("Если возраст человека равен "+age+" то он совершеннолетний");}
+        else {System.out.println("Если возраст человека равен "+age+" то он не достиг совершеннолетия, нужно немного подождать");}
+
     }
 }
